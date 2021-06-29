b744a30b1b30da68d83de90fa441ffd694739c15
emdad118661 <emdad118661@gmail.com>
2021-06-29 21:54:36 +0600

    project view 2nd

---
+++ Project/view/contact.php
@@ -0,0 +1,117 @@
+<html>
+
+<head>
+	<title> Contact Us | Leaf </title>
+</head>
+
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<td>
+				<img src="Companylogo.png" alt="logo" width="100px" height="50px">
+			</td>
+			
+			<td align="right">
+				Loggedin as  | <a href="login.php"> Logout</a>
+			</td>
+		</tr>
+		
+		<tr style="height:150px;">
+			<td>
+				<h4>Account</h4><br>
+                <ul>
+                    <li>
+                        <a href="home.php">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="viewProfile.php">View Profile</a>
+                    </li>
+                    <li>
+                        <a href="EditProfile.php">Edit Profile</a>
+                    </li>
+                    <li>
+                        <a href="ProfilePic.php">Change Profile Picture</a>
+                    </li>
+                    <li>
+                        <a href="ChangePass.php">Change Password</a>
+                    </li>
+                    <li>
+                        <a href="article.php">Articles</a>
+                    </li>
+                    <li>
+                        <a href="events.php">Events</a>
+                    </li>
+                    <li>
+                        <a href="news.php">News</a>
+                    </li>
+                    <li>
+                        <a href="gallary.php">Gallary</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="expertList.php">Expert List</a>
+                    </li>
+                    <li>
+                        <a href="community.php">Our Community</a>
+                    </li>
+                    <li>
+                        <a href="qna.php">QnA</a>
+                    </li>
+                    <li>
+                        <a href="message.php">Messages</a>
+                    </li>
+                    <li>
+                        <a href="courses.php">Courses</a>
+                    </li>
+                    <li>
+                        <a href="helpline.php">Emergency Helpline</a>
+                    </li>
+                    <li>
+                        <a href="research.php">Research</a>
+                    </li>
+                    <li>
+                        <a href="weather.php">Weather Update</a>
+                    </li>
+                    <li>
+                        <a href="donate.php">Donate</a>
+                    </li>
+                    <li>
+                        <a href="Help.php">Help & Suggestion</a>
+                    </li>
+                    <li>
+                        <a href="DeleteAccount.php">Delete Account</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Logout</a>
+                    </li>
+                </ul>
+			</td>
+            <td> 
+                <h4>Contact on: </h4><br>
+                <ul>
+                    <li>
+                        Phone: 02********, 02*********
+                    </li>
+                    <li>
+                        Mobile: 017******, 019******
+                    </li>
+                </ul> <br>
+                <h4>For Emergency:</h4>
+                <ul>
+                    <li>
+                        Call: *437#
+                    </li>
+                </ul>       
+            </td>
+		</tr>
+		
+		<tr>
+			<td colspan="2" align="center">
+				18-39032-3@student.aiub.edu
+			</td>
+		</tr>			
+	</table>
+</body>
+</html>
---
+++ Project/view/news.php
@@ -0,0 +1,157 @@
+<html>
+
+<head>
+	<title> News | Leaf </title>
+</head>
+
+<body>
+	<table border="1" width="100%">
+		<tr>
+			<td>
+				<img src="Companylogo.png" alt="logo" width="100px" height="50px">
+			</td>
+			
+			<td align="right">
+				Loggedin as  | <a href="login.php"> Logout</a>
+			</td>
+		</tr>
+		
+		<tr style="height:150px;">
+			<td>
+				<h4>Account</h4><br>
+                <ul>
+                    <li>
+                        <a href="home.php">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="viewProfile.php">View Profile</a>
+                    </li>
+                    <li>
+                        <a href="EditProfile.php">Edit Profile</a>
+                    </li>
+                    <li>
+                        <a href="ProfilePic.php">Change Profile Picture</a>
+                    </li>
+                    <li>
+                        <a href="ChangePass.php">Change Password</a>
+                    </li>
+                    <li>
+                        <a href="article.php">Articles</a>
+                    </li>
+                    <li>
+                        <a href="events.php">Events</a>
+                    </li>
+                    <li>
+                        <a href="news.php">News</a>
+                    </li>
+                    <li>
+                        <a href="gallary.php">Gallary</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="expertList.php">Expert List</a>
+                    </li>
+                    <li>
+                        <a href="community.php">Our Community</a>
+                    </li>
+                    <li>
+                        <a href="qna.php">QnA</a>
+                    </li>
+                    <li>
+                        <a href="message.php">Messages</a>
+                    </li>
+                    <li>
+                        <a href="courses.php">Courses</a>
+                    </li>
+                    <li>
+                        <a href="helpline.php">Emergency Helpline</a>
+                    </li>
+                    <li>
+                        <a href="research.php">Research</a>
+                    </li>
+                    <li>
+                        <a href="weather.php">Weather Update</a>
+                    </li>
+                    <li>
+                        <a href="donate.php">Donate</a>
+                    </li>
+                    <li>
+                        <a href="Help.php">Help & Suggestion</a>
+                    </li>
+                    <li>
+                        <a href="DeleteAccount.php">Delete Account</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Logout</a>
+                    </li>
+                </ul>
+			</td>
+            <td> 
+                <h1>Top 10 Successfull Farmers</h1>
+                <p>Over the course of each year, Successful Farming magazine editors talk to hundreds of farmers. Some on the phone to get updates on crop progress in their areas, some via Twitter to hear what farmers think of the latest policy changes, and some on the farm. These are the best, in my opinion, because this is when you get to see a farmer in his or her element. This is when you can best understand who they are as a farmer, innovator, businessman, environmentalist, parent, and more.</p>
+
+                <p>From these interviews, emerge a plethora of stories. While the approach for each story differs, the goal is the same: To share the lessons learned from one farmer so they can help another. </p>
+
+                <p>For this list, we asked our editors to pick the most innovative farmers they covered this year. We didn’t use a formula to prove their success; we didn’t have a scorecard to determine who made the list or who didn’t. We chose these 10 farmers because they’re innovators who are making a difference in the industry, starting with their own operations. In no particular order, here's who made the list:</p> 
+
+                <p>1. TREY HILL FROM ROCK HALL, MARYLAND<br>
+                   Maryland farmer Trey Hill uses cover crops on all of his 13,000 acres and he plants green into the majority of them. And he does it without sacrificing yields.
+
+                   “The environment has become a priority that factors into most of our decisions,” he says. Read Hill’s story.</p>
+                <p>2. DOUG TOUSSAINT (+ HIS SONS NICK AND BRAD) FROM WAHPETON, NORTH DAKOTA<br>
+                   The Toussaint’s are another family that go against the grain – they also plant into green cover crops.
+
+                   “We just go in with no special attachments on our planter and plant,” says Nick. “It looks amazing. The soils are so mellow that it’s like planting into a garden.” Learn more about the Toussaints.</p>
+                <p>3. SHELBY SMITH FROM AMES, IOWA<br>
+                   In September 2017, Smith handed in her resignation to the bank she’d been working at in Dublin, Ireland, and returned to her family’s row-crop farm in Iowa. Her father told her, “If you don’t want to fight the whole corn and soybean thing like I have for the last 30-plus years and want to come up with a niche, go for it. We’ll help you get started.”
+
+                   As bizarre as it might seem, that niche needed up being crickets. Learn about Smith’s 250,000-population cricket farm.</p>
+                <p>4. BILL PRESTAGE FROM CLINTON, NORTH CAROLINA<br>
+                   You can always count on Bill Prestage, 82, to tell it like it is. Family-owned Prestage Farms, 35 years in business, is the sixth largest pork producer in the U.S., with 175,000 sows, and a vertically integrated turkey producer.
+
+                   “You are either building and going ahead or you’re standing still and sinking. I think you’ve got to keep on going. That, to me, is the fun part of business,” he says. Read Prestage’s story.</p>
+                <p>5. BILL BEAM FROM ELVERSON, PENNSYLVANIA<br>
+                   Is there anything Beam won’t try on his farm? “I won’t do anything illegal,” he jokes.
+
+                   Beam loves to experiment with new businesses and crops. Beyond his core crops – soybeans, corn, and wheat – he runs a sawdust business, grows non-GMO corn and high-oleic soybeans, and, for the first time this year, he’s trying his hand at hemp. Read Beam’s story.</p>
+                <p>6. LORA HOWELL FROM DANVILLE, OHIO<br>
+                   When Howell suddenly found herself leading the family’s farm three years ago, she was faced with some pretty difficult decisions.
+
+                   “My husband, Dave, and I purchased our 500-acre farm more than 25 years ago and built it from the ground up,” Howell says. “We were living our dream. When he passed away, I had to decide what I was going to do with the farm.”
+
+                   To build a future for the farm and her children, Howell decided to adopt new precision ag tools. Read her story.</p>
+                <p>7. STEVE REIMER FROM CHAMBERLAIN, SOUTH DAKOTA<br>
+                   Storing water in the soil has been Reimer’s mission for more than four decades. His three-pronged management strategy includes leaving residue on the surface, incorporating cover crops into the rotation, and grazing cow-calf pairs on cover crops.
+
+                   “Back in the 1970s, we grew corn and small grains, and we also did tillage summer fallow. We were seeing water running off the fields and taking topsoil with it. We wanted, instead, for the water to stay where it fell,” he says. Read Reimer’s story.</p>
+                <p>
+                    8. THE CHAPPELL BROTHERS (ADAM AND SETH) FROM COTTON PLANT, ARKANSAS<br>
+                    Five years ago, Adam and Seth were on a treadmill that offered little hope of escape. Farming 8,000 acres, the Chappells struggled with an invasion of the herbicide-resistant weed Palmer amaranth.
+
+                    The brothers made changes that branded them as “nuts” in their community. Today, they grow a diverse rotation of non-GMO corn, cotton, soybeans, and rice. Breaking from a tradition of tillage, they now no-till their fields and plant every acre to a cover crop. Read more of the Chappell’s story.
+                </p>
+                <p>
+                    9. BRIAN SCOTT FROM DELPHI, INDIANA<br>
+                    Innovation comes in multiple forms, including how farmers share their story with the world. While farming with his father and grandfather, Scott shares their experiences on Facebook and other social media platforms. Since launching his Facebook page, The Farmer’s Life, in February 2011, Scott has attracted nearly 60,000 followers.
+
+                    “I tell a lot of people, whatever your post doesn't have to be the most detailed end all, be all, post of all time. People that are interested in learning about farming will be surprised to find out the small little things, mundane stuff we do every day. It's very interesting to somebody who's got no experience with it.” Read Scott’s story.
+                </p>
+                <p>
+                    10. MATTHEW SLIGAR FROM GRIDLEY, CALIFORNIA<br>
+                    In 2016, Sligar launched Rice Farming TV – a YouTube channel that shows how medium-grain rice is grown. Each episode documents one part of the process, from planting to draining fields to harvesting.
+
+                    “I really enjoy the creative process,” he says. “I enjoy the engagement from the audience. The first day a video is live, I can get 50 comments on YouTube and another 20 engagements on Facebook or via email.” Read his story as well as those of three other farmers who are using video in unique ways.
+                </p>
+            </td>
+		</tr>
+		
+		<tr>
+			<td colspan="2" align="center">
+				18-39032-3@student.aiub.edu
+			</td>
+		</tr>			
+	</table>
+</body>
+</html>
